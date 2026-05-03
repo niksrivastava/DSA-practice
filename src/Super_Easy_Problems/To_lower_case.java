84287ce50b889a10e60803c248b9ab7157de2333
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-05-04 01:23:45 +0530

    Solved ques - To_lower_case

---
+++ src/Super_Easy_Problems/To_lower_case.java
@@ -0,0 +1,13 @@
+package Super_Easy_Problems;
+
+public class To_lower_case {
+    public static void main(String[] args) {
+        String s = "HELLO";
+        System.out.println(lower_case(s));
+    
+    }
+    static String lower_case(String s){
+        
+        return s.toLowerCase();
+    }
+}
