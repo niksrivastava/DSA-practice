3455acef2a170846d78e0948f2ab0fc29473c708
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-05-06 03:49:06 +0530

    Solved ques - Median_of_two_sorted_arr

--- src/Binary_Search/Median_of_two_sorted_arr.java
+++ src/Binary_Search/Median_of_two_sorted_arr.java
@@ -2,30 +2,49 @@ package Binary_Search;
 import java.util.Arrays;
 public class Median_of_two_sorted_arr {
     public static void main(String[] args) {
-        int[] num1 = {1,2};
-        int[] num2 = {3,4};
-        System.out.println(median(num1, num2));
+        int[] nums1 = {1,3};
+        int[] nums2 = {2};
+        System.out.println(median(nums1, nums2));
     }
-    static double median(int[] num1, int[] num2){
+    static double median(int[] nums1, int[] nums2){
+        int[] nums3 = new int[nums1.length + nums2.length];
+        int nums1_s = 0;
+        int nums2_s = 0;
         double med = 0;
-        int[] nums3 = new int[num1.length+num2.length];
-        int s = 0;
-        int e = nums3.length-1;
-        // System.out.println(num3.length);
-        int m = s + (e-s)/2;
-        // System.out.println(m);
-        for (int i = 0; i < nums3.length-1; i++) {
-            if(nums3[i]>nums3[i+1]){
-                nums3[i]=nums3[i+1];
+        int i = 0;
+        while( nums1_s < nums1.length && nums2_s < nums2.length){
+            if(nums1[nums1_s] > nums2[nums2_s]){
+                nums3[i] = nums2[nums2_s];
+                nums2_s++;
+            }else{
+                nums3[i] = nums1[nums1_s];
+                nums1_s++;
             }
+            i++;
         }
         System.out.println(Arrays.toString(nums3));
-        // if(nums3.length%2 != 0){
-        //     med = nums3[m];
-        // }else{
-        //     med = m+(m+1);
-        //     System.out.println(med);
-        // }
+        while(nums1_s<nums1.length){
+            nums3[i] = nums1[nums1_s];
+            nums1_s++;
+            i++;
+        }
+
+        while(nums2_s<nums2.length){
+            nums3[i] = nums2[nums2_s];
+            nums2_s++;
+            i++;
+        }
+
+        int s = 0;
+        int e = nums3.length-1;
+        int m = s + (e-s)/2;
+
+        if(nums3.length%2 != 0){
+            med = nums3[m];
+        }else{
+            med = (nums3[m]+nums3[m-1])/2.0;
+
+        }
         return med;
     }
 }
