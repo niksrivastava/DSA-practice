4eff485d82dd0cfc0cc2dab07a213386a90f4883
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-05-17 07:33:48 +0530

    Palindrome

--- src/String_And_StringBuilder/Palindrome.java
+++ src/String_And_StringBuilder/Palindrome.java
@@ -1,22 +1,49 @@
 package String_And_StringBuilder;
+
+import java.util.Arrays;
+
 public class Palindrome {
     public static void main(String[] args) {
-        String str = "";
-        System.out.println(isPalin(str));
+        String str = "madm";
+        System.out.println(isPalindrome(str));
     }
-    static boolean isPalin(String str){
+
+    static boolean isPalindrome(String str){    
         if(str == null || str.length() == 0){
             return true;
         }
         str = str.toLowerCase();
-        for(int i = 0; i <= str.length() / 2; i++){
-            char start = str.charAt(i);
-            char end = str.charAt(str.length() - 1 -i);
 
-            if(start != end){
+        int s = 0;
+        int e = str.length()-1;
+
+        
+        while (s<e) {
+            if(str.charAt(s) != str.charAt(e)){
                 return false;
             }
+            s++;
+            e--;
+            
         }
         return true;
+
     }
+
+
+    // static boolean isPalin(String str){
+    //     if(str == null || str.length() == 0){
+    //         return true;
+    //     }
+    //     str = str.toLowerCase();
+    //     for(int i = 0; i <= str.length() / 2; i++){
+    //         char start = str.charAt(i);
+    //         char end = str.charAt(str.length() - 1 -i);
+
+    //         if(start != end){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 }
