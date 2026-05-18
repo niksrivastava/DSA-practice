d0c6c0b414b8fab63b0712354ac377638b3cd177
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-05-19 01:53:26 +0530

    Upper_bound

--- src/Binary_Search/Upper_bound.java
+++ src/Binary_Search/Upper_bound.java
@@ -3,7 +3,7 @@ package Binary_Search;
 public class Upper_bound {
     public static void main(String[] args) {
         int arr[] = {1,1,2,2,2,2,3};
-        int ans = occur(arr, 3);
+        int ans = occur(arr, 1);
         System.out.println(ans);
     }
     static int occur(int[] nums, int target){
@@ -12,16 +12,15 @@ public class Upper_bound {
         int m = s+ (e-s)/2;
         int ans = -1;
         while(s<=e){
-            if(target <= nums[m]){
+            if(target >= nums[m]){
+                
+                s = m+1;;
+            }
+            if(target < nums[m]){
                 ans = m;
-                e = m -1;
+                e = m-1;
             }
-            if(target > nums[m]){
-                s = m+1;
-            }
-            // if(target< nums[m]){
-            //     e=m-1;
-            // }
+          
             m = s + (e-s)/2;
         }
         return ans;
