4147db5df755796649b1fc5aef27a17707cefbad
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-05-29 03:48:38 +0530

    Sieve_of_Eratosthenes

---
+++ src/Super_Easy_Problems/Sieve_of_Eratosthenes.java
@@ -0,0 +1,23 @@
+package Super_Easy_Problems;
+
+public class Sieve_of_Eratosthenes {
+    public static void main(String[] args) {
+        int n = 40;
+        boolean [] arr = new boolean[n + 1];
+        sieve(n,arr);
+    }
+    static void sieve(int n, boolean[] primes){
+        for (int i = 2; i*i<= n; i++) {
+            if(primes[i] == false){
+                for (int j = i*2; j <= n; j+=i) {
+                    primes[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i < primes.length; i++) {
+            if(primes[i]==false){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
