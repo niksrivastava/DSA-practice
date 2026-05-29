327353ec79abe9ca92863c6141fd9fe3a6cdc09a
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-05-30 01:55:05 +0530

    LCM

---
+++ src/Recursion/LCM.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class LCM {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 34;
+        int ans = (a*b)/gcd(a,b);
+        System.out.println(ans);
+    }
+    static int gcd(int a, int b){
+        if(a==0){
+            return b;
+
+        }
+        return gcd(b%a, a);
+    }
+}
