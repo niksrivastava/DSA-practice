02f778b7cbc5abe134ea7f68df2a025833e561cf
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-05-30 06:04:46 +0530

    LCM

--- src/Recursion/LCM.java
+++ src/Recursion/LCM.java
@@ -1,7 +1,7 @@
 package Recursion;
 
 public class LCM {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int a = 17;
         int b = 34;
         int ans = (a*b)/gcd(a,b);
