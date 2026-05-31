b5f3e5ff7dd0054c80c93304021ea8aac7164dd2
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-05-31 12:51:39 +0530

    added find_all_index functions to Linear_search_with_recursion

--- src/Recursion/Linear_search_with_recursion.java
+++ src/Recursion/Linear_search_with_recursion.java
@@ -1,11 +1,16 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class Linear_search_with_recursion {
     public static void main(String[] args) {
         int arr[] = {1,2,3523,4,853};
         System.out.println(LS_value(arr,0,0)+"," + " Which means: " +LS_bool(arr, 0, 0));
-
+        int arr2[] = {1,2,4,6,8,5,2,6};
+        System.out.println(find_all_index(arr2, 0, 2, new ArrayList<>()));
+        System.out.println(find_all_index(arr2, 0, 2));
     }
+
     static int LS_value(int arr[] , int index, int target){
         if(index == arr.length){
             return -1;
@@ -18,11 +23,44 @@ public class Linear_search_with_recursion {
         return LS_value(arr, index + 1, target);
     }
 
-        static boolean LS_bool(int arr[] , int index, int target){
+    static boolean LS_bool(int arr[] , int index, int target){
         if(index == arr.length){
             return false;
         }
 
         return arr[index] == target || LS_bool(arr, index + 1, target);
     }
+
+    static ArrayList<Integer> find_all_index(int arr[] , int index, int target, ArrayList<Integer> list){
+        if(index == arr.length){
+            return list;
+        }
+
+        if(arr[index] == target){
+            list.add(index);
+        }
+
+        return find_all_index(arr, index + 1, target, list);
+    }
+
+    static ArrayList<Integer> find_all_index(int arr[] , int index, int target){
+        
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if(index == arr.length){
+            return list;
+        }
+
+        if(arr[index] == target){
+            list.add(index);
+        }
+
+        ArrayList<Integer> ans_from_below_calls = find_all_index(arr, index + 1, target);
+
+        list.addAll(ans_from_below_calls);
+
+        return list;
+    }
+
+    
 }
