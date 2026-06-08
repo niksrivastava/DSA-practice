d2b6a8265364ae9639fecb811e4893b2555c6535
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-06-08 15:34:53 +0530

    added another function in SubSeq_with_recursion

--- src/String_And_StringBuilder/SubSeq_with_recursion.java
+++ src/String_And_StringBuilder/SubSeq_with_recursion.java
@@ -1,10 +1,14 @@
 package String_And_StringBuilder;
 
+import java.util.ArrayList;
+
 public class SubSeq_with_recursion {
     public static void main(String[] args) {
         sub_seq("", "abc");
+        System.out.println(sub_seq_return("", "abc"));
         
     }
+
     static void sub_seq(String p, String up){
         if(up.isEmpty()){
             System.out.println(p);
@@ -14,4 +18,24 @@ public class SubSeq_with_recursion {
         sub_seq(p + ch, up.substring(1));
         sub_seq(p, up.substring(1));
     }
+
+    static ArrayList<String> sub_seq_return(String p, String up){
+        
+        ArrayList<String> list = new ArrayList<>();
+
+        if(up.isEmpty()){
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = sub_seq_return(p + ch, up.substring(1));
+        ArrayList<String> right = sub_seq_return(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
+    }
+
+
 }
