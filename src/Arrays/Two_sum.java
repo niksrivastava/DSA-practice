ebac264ce30b18581fa00b607453841992a8ce1c
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-06-13 12:10:02 +0530

    Two_sum

--- src/Arrays/Two_sum.java
+++ src/Arrays/Two_sum.java
@@ -8,19 +8,13 @@ public class Two_sum {
         System.out.println(Arrays.toString(ans));
     }
     static int[] sum(int[] nums, int target){
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if(i==j){
-                    continue;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i,j};
                 }
-                sum = nums[i] + nums[j];
-                if(sum == target){
-                    return new int [] {i+1, j+1};
-                } 
-                sum = 0;  
             }
         }
-         return new int[]{};
+        return new int[]{};
     }
 }
