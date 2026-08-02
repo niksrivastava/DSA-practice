ef7b5d4284823fc99e073e76fa0006149bccfd5b
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-08-02 11:47:30 +0530

    feat: add optimised three_sum method

--- src/Arrays/Three_sum.java
+++ src/Arrays/Three_sum.java
@@ -6,39 +6,105 @@ import java.util.List;
 
 public class Three_sum {
     public static void main(String[] args) {
-        int[] nums = {-1, 0, 1, 2, -1, -4};
 
-        List<List<Integer>> result = threeSum(nums);
+        int nums[] = {-1,0,1,2,-1,-4};
+
+        List<List<Integer>> result = three_sum(nums);
+
+        List<List<Integer>> optimised = three_sum_opt(nums);
 
         System.out.println(result);
+
+        System.out.println(optimised);
+    
     }
-    static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
 
-        Arrays.sort(nums);
-        int n = nums.length;
+    public static List<List<Integer>> three_sum(int[] arr){
 
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
+        List<List<Integer>> result = new ArrayList<>();
 
-                    if (nums[i] + nums[j] + nums[k] == 0) {
+        Arrays.sort(arr);
+        int n = arr.length;
 
-                        List<Integer> temp = Arrays.asList(
-                                nums[i],
-                                nums[j],
-                                nums[k]
-                        );
+        for(int i = 0; i < n-2; i++){
+            for(int j = i+1; j < n-1; j++){
+                for(int k = j+1; k < n; k++){
+                    if(arr[i] + arr[j] + arr[k] == 0){
 
-                        if (!ans.contains(temp)) {
-                            ans.add(temp);
-                        }
+                        List<Integer> ans = new ArrayList<>();
+                        ans.add(arr[i]);
+                        ans.add(arr[j]);
+                        ans.add(arr[k]);
+                    
+                    if(result.contains(ans) == false){
+                        result.add(ans);
                     }
                 }
             }
         }
+    }
+        return result;
+    }
 
-        return ans;
+    public static List<List<Integer>> three_sum_opt(int arr[]){
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(arr);
+        int n = arr.length;
+
+        int target = 0;
+
+        for(int i = 0; i < n-2; i++){
+
+            if(i > 0 && arr[i] == arr[i-1]){
+                continue;
+            }
+
+            if(arr[i] > 0){
+                break;
+            }
+
+            target = -1*arr[i];
+
+            int left = i+1;
+            int right = n-1;
+            
+            while(left < right){
+                int sum = arr[left] + arr[right]; 
+                if(sum == target){
+
+                    // // Important: Arrays.asList() se bani list fixed-size hoti hai. Isme add() ya remove() nahi kar sakte:
+
+                    List<Integer> ans = Arrays.asList(
+                        arr[i],
+                        arr[left],
+                        arr[right]
+                    );
+
+                    result.add(ans);
+
+                    left++;
+                    right--;
+
+                
+
+                while(left < n && arr[left] == arr[left-1]){
+                    left++;
+                }
+
+                while(right >= 0 && arr[right] == arr[right+1] ){
+                    right--;
+                }
+
+            }else if(sum < target){
+                    left++;
+                }
+                else{
+                    right--;
+                }  
+            }
+        }
+    
+        return result;
     }
 }
 
