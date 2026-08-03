0bed7e2acad17e8bb10a5620e5d72f052c78c5ca
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-08-03 17:25:08 +0530

    add Four_Sum

--- src/Arrays/swap012.java
+++ src/Arrays/swap012.java
@@ -1,36 +1,41 @@
 package Arrays;
 import java.util.*;
 public class swap012 {
+    public static void main(String[] args) {
+        int arr[] = {0, 2, 2, 1, 0, 1, 1, 0, 2};
+
+        int ans[] = sort(arr);
+        System.out.println(Arrays.toString(ans)); 
+    }
+    
+    public static int[] sort(int arr[]){
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+
+        while(mid<=high){
+            if(arr[mid] == 0){
+                swap(arr, mid,low);
+                mid++;
+                low++;
+               
+            }
+            else if(arr[mid] == 1){
+                
+                mid++;
+            }
+            else{
+                swap(arr, mid,high);
+                high--;
+            }
+        }
+        return arr;
+    }
+
 
     public static void swap(int arr[], int i , int j){
         int t = arr[i];
         arr[i] = arr[j];
         arr[j] = t;    
-    }
-
-    public static void main(String[] args) {
-        int arr[] = {0, 2, 2, 1, 0, 1, 1, 0, 2};
-        int left = 0;
-        int right = arr.length -1;
-        int mid = 0 ;
-        while(mid<=right ){
-            if(arr[mid]==0){
-                swap(arr,left,mid);
-                left++;
-                mid++;
-            }
-            else if(arr[mid]==1){
-                mid++;
-            }
-            else{ // arr[mid]==2;
-                swap(arr, mid, right);
-                
-                right--;
-            }
-
-        }
-        for(int i = 0; i< arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
     }    
 }
