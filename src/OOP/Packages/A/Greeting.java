eb75210d45c798ccfabdeded6154bb20de4c5d70
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-08-11 23:51:08 +0530

    Add Java packages examples

---
+++ src/OOP/Packages/A/Greeting.java
@@ -0,0 +1,18 @@
+package OOP.Packages.A;
+
+import static OOP.Packages.B.Message.message;
+
+import static OOP.Introduction.LearningPackages.learning;
+
+import static HashMap.LearningPackages.*;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        message();
+        learning();
+        learning2();
+
+
+    }
+}
---
+++ src/OOP/Packages/B/Greeting.java
@@ -0,0 +1,10 @@
+package OOP.Packages.B;
+
+import static OOP.Packages.B.Message.message;;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Learning oop");
+        message();
+    }
+}
