ef4e130f2a1f077c9c7a93677293b6d24c4e9632
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-08-17 03:30:31 +0530

    feat: alternate method for Duplicate Zeros Not for In Place problem

--- src/DsaPatterns/TwoPointers/DuplicateZeros.java
+++ src/DsaPatterns/TwoPointers/DuplicateZeros.java
@@ -4,22 +4,27 @@ import java.util.*;
 
 public class DuplicateZeros {
     public static void main(String[] args) {
-        int arr[] = {1,0,2,3,0,4,5,0};
+        int arr[] = {0,0,0,0,0,0,0};
         int nums[] = duplicateZeros_notInPlace(arr);
+        int num[] = duplicateZeros_InPlace(arr);
         System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(num));
     }    
 
     public static int[] duplicateZeros_notInPlace(int arr[]){
         int nums[] = new int[arr.length];
-        int k =0;
+        int k = 0;
 
         for(int i = 0;i < arr.length;i++){
             if(arr[i] != 0){
                 nums[k] = arr[i];
                 k++;
-            }else if(arr[i] == 0){
+            }else{
                 nums[k] = arr[i];
-                nums[k+1] = arr[i];
+                if(k+1 < arr.length){
+                    nums[k+1] = arr[i];
+                }
+                
                 k += 2;
             }
             if(k >= nums.length){
@@ -27,5 +32,23 @@ public class DuplicateZeros {
             }
         }
         return nums;
+    }
+    public static int[] duplicateZeros_InPlace(int arr[]){
+
+        for(int i =0; i < arr.length; i++){
+            if(arr[i] == 0){
+                for(int j = arr.length-1; j > i; j--){
+                    arr[j] = arr[j-1];
+                }
+
+                if(i < arr.length-1){
+                    arr[i+1] = 0;
+                    i++;
+                }
+                
+                
+            }
+        }
+        return arr;
     }
 }
