2fa671827c32fb177d7514f8997c957de0d192c2
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-08-18 07:48:18 +0530

    solve: Reverse String 2 problem

--- src/DsaPatterns/TwoPointers/ReverseString2.java
+++ src/DsaPatterns/TwoPointers/ReverseString2.java
@@ -2,12 +2,39 @@ package DsaPatterns.TwoPointers;
 
 public class ReverseString2 {
     public static void main(String[] args) {
-        String s = "abcdefg";
-        int k = 2;
+        String s = "hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl";
+        int k = 8;
         String ans = revString(s, k);
         System.out.println(ans);
     }
     public static String revString(String s, int k){
+
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = Math.min(i+k-1, arr.length-1);
+
+        while(i < arr.length){
+            int n = i;
+            int m = j; 
+
+            while(n<m){
+                swap(arr, n++, m--);
+            } 
+    
+            i = 2*k + i;
+            j = Math.min(i+k-1, arr.length-1);
+            
+        }
+
+
+        s = new String(arr);
+
         return s;
+    }
+
+    public static void swap(char[] arr, int i, int j){
+        char t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
     }
 }
