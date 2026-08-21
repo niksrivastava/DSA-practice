f86780b09a6efb2b9c07aac5a74f610baf1ebf45
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-08-21 16:18:23 +0530

    chore: rename modified files

---
+++ src/Strings/Stream_with_recursion.java
@@ -0,0 +1,45 @@
+package Strings;
+
+public class Stream_with_recursion {
+    public static void main(String[] args) {
+        skip("", "baccdah");
+        System.out.println(skip( "baccdah"));
+        
+
+        
+    }
+
+    static void skip(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        if(ch == 'a'){
+            skip(p, up.substring(1));
+        }
+        else{
+            skip(p + ch, up.substring(1));
+        }
+
+    }
+
+    static String skip(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+
+        char ch = up.charAt(0);
+
+        if(ch == 'a'){
+            return skip(up.substring(1));
+        }
+        else{
+            return ch + skip(up.substring(1));
+        }
+
+    }
+
+}
