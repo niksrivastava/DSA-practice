b9201460aacc84c93612782aac6b2519b248c59f
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-08-27 18:08:18 +0530

    add: optimized soltion of Find the K-Beauty of a Number

--- src/DsaPatterns/SlidingWindow/FindTheKBeautyOfNumber.java
+++ src/DsaPatterns/SlidingWindow/FindTheKBeautyOfNumber.java
@@ -5,7 +5,9 @@ public class FindTheKBeautyOfNumber {
         int num = 430043;
         int k = 2;
         int ans = kBeauty(num, k);
+        int optimizedAns = optimizedKBeauty(num, k);
         System.out.println(ans);
+        System.out.println(optimizedAns);
     }
 
     public static int kBeauty(int num, int k){
@@ -27,6 +29,26 @@ public class FindTheKBeautyOfNumber {
                 }
             }catch(Exception e){}
             
+            low++;
+            high++;
+        } 
+        return beauty;
+    }
+    public static int optimizedKBeauty(int num, int k){
+        int beauty = 0;
+        String s = String.valueOf(num);
+        int low = 0;
+        int high = k -1;
+
+        while(high < s.length()){
+
+            String divisor = s.substring(low, low + k);
+            int div = Integer.parseInt(divisor);
+
+            if(div != 0 && num%div == 0){
+                beauty++;
+            }
+
             low++;
             high++;
         } 
