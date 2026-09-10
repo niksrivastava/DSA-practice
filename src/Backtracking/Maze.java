e5f4e332999d78a80fc2f8681866ed77ae84e26f
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-10 12:25:26 +0530

    feat: add path generation for maze problem

--- src/Backtracking/Maze.java
+++ src/Backtracking/Maze.java
@@ -4,6 +4,7 @@ public class Maze {
     public static void main(String[] args) {
 
         System.out.println(count(3, 3));
+        path("", 3, 3);
         
     }
     public static int count(int r, int c){
@@ -15,5 +16,20 @@ public class Maze {
         int right = count(r, c-1);
 
         return left + right;
+    }
+
+    public static void path(String p, int r, int c){
+        if(r == 1 && c == 1){
+            System.out.println(p);
+            return;
+        }
+
+        if(r > 1){
+            path(p+'D', r-1, c);
+        }
+
+        if(c > 1){
+            path(p+'R', r, c-1);
+        }
     }
 }
