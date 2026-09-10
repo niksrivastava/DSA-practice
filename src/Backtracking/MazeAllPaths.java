8ff5c10fcdd0d09166ada90cb90aea54ef7c2463
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-10 13:56:38 +0530

    learn: implement maze using backtracking

---
+++ src/Backtracking/MazeAllPaths.java
@@ -0,0 +1,48 @@
+package Backtracking;
+
+public class MazeAllPaths {
+    public static void main(String[] args) {
+        boolean board[][] = {
+            {true, true, true},
+            {true, true, true},
+            {true, true, true},
+        }; 
+
+        allPath("", board, 0, 0);
+    }
+
+    public static void allPath(String p, boolean [][] maze, int r, int c){
+
+        if(r == maze.length-1 && c == maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        // I am considering this block in my path
+        maze[r][c] = false;
+
+        if(r < maze.length-1){
+            allPath(p+'D', maze, r+1, c);
+        }
+
+        if(c < maze[0].length-1){
+            allPath(p+'R', maze, r, c+1);
+        }
+
+        if(r > 0){
+            allPath(p+'U', maze, r-1, c);
+        }
+
+        if(c > 0){
+            allPath(p+'L', maze, r, c-1);
+        }
+
+        // This line is where  the function will be over
+        // So, before the function, also remove the changes that were made by that function.
+        maze[r][c] = true;
+    }
+}
