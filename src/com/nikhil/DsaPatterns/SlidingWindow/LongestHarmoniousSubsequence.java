ebf1a4924a0cb6a8967765114369b4cb3bc41f66
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-15 17:37:02 +0530

    Add alternate approach: Longest Harmonious Subsequence

--- src/com/nikhil/DsaPatterns/SlidingWindow/LongestHarmoniousSubsequence.java
+++ src/com/nikhil/DsaPatterns/SlidingWindow/LongestHarmoniousSubsequence.java
@@ -1,10 +1,14 @@
 package com.nikhil.DsaPatterns.SlidingWindow;
 
+import java.util.Arrays;
+
 public class LongestHarmoniousSubsequence {
     public static void main(String[] args) {
-        int arr [] = {1,3,2,2,5,2,3,7};
+        int arr [] = {1,2,2,1};
         int ans = harmonious(arr);
         System.out.println(ans);     
+        int optimizedAns = optimizedHarmonious(arr);
+        System.out.println(optimizedAns);
     }
 
     public static int harmonious(int arr[]){
@@ -62,6 +66,44 @@ public class LongestHarmoniousSubsequence {
             low++;
         }
 
+        return maxAns;
+
+    }
+
+    public static int optimizedHarmonious(int arr[]){
+        Arrays.sort(arr);
+        int ans = 0;
+        int maxAns = 0;
+        int low = 0;
+        int high = 1;
+        int zero = 1;
+        int one = 0;
+
+        while(high < arr.length){
+            int diff = Math.abs(arr[low] - arr[high]);
+            if(diff == 1){
+                one++;
+                high++;
+                if(one > 0){
+                    ans = one + zero;
+                    maxAns = Math.max(maxAns, ans);
+                }
+                else{
+                    maxAns = Math.max(maxAns, ans);
+                }
+            }
+            else if(diff == 0){
+                zero++;
+                high++;
+            }
+            else{
+                one = 0;
+                zero = 1;
+                ans = 0;
+                low++;
+                high = low + 1;
+            }
+        }
         return maxAns;
 
     }
