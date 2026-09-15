336bf2b49c9035a85d65302db50a855453c20f86
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-16 03:56:20 +0530

    Add better approach: Trapping rain water

--- src/com/nikhil/DsaPatterns/TwoPointers/TrappingRainWater.java
+++ src/com/nikhil/DsaPatterns/TwoPointers/TrappingRainWater.java
@@ -2,18 +2,20 @@ package com.nikhil.DsaPatterns.TwoPointers;
 
 public class TrappingRainWater {
     public static void main(String[] args) {
-        int arr[] = {4,2,3};
+        int arr[] = {4,2,0,3,2,5};
         int ans = trap(arr);
         System.out.println(ans);
+        int betterAns = better(arr);
+        System.out.println(betterAns);
+        
     }
     public static int trap(int arr[]){
-        int ans = 0;
 
+        int ans = 0;
 
         for (int i = 0; i < arr.length; i++) {
 
-            int leftMax = 0;
-            
+            int leftMax = 0;            
             
             for (int j = 0; j <= i; j++) {
                 leftMax = Math.max(leftMax, arr[j]);
@@ -24,12 +26,35 @@ public class TrappingRainWater {
             for (int j = i; j < arr.length; j++) {
 
                 rightMax = Math.max(rightMax, arr[j]);
-                
             }
             ans += Math.min(leftMax, rightMax) - arr[i];
         }
 
         return ans;
+    }
 
+    public static int better(int arr[]){
+
+        int ans = 0;
+        int n = arr.length;
+        int lMax[] = new int[arr.length];
+        int rMax[] = new int[arr.length];
+
+        lMax[0] = arr[0];
+        rMax[n-1] = arr[n-1];
+
+        for (int i = 1; i < n; i++) {
+            lMax[i] = Math.max(lMax[i-1], arr[i]);
+        }
+
+        for (int i = n-2; i >= 0; i--) {
+            rMax[i] = Math.max(rMax[i+1], arr[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(lMax[i], rMax[i]) - arr[i];
+        }
+
+        return ans;
     }
 }
