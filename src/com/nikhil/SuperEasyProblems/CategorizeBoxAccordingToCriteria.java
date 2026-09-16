0da0235049b02c8e4cdc1abd93a824c1ac1cee47
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-16 06:17:41 +0530

    Solve: Categorize Box According to Criteria

---
+++ src/com/nikhil/SuperEasyProblems/CategorizeBoxAccordingToCriteria.java
@@ -0,0 +1,43 @@
+package com.nikhil.SuperEasyProblems;
+
+public class CategorizeBoxAccordingToCriteria {
+    public static void main(String[] args) {
+        int length = 2909; int width = 3968; int height = 3272; int mass = 727;
+        String ans = category(length, width, height, mass);
+        System.out.println(ans);
+    }
+
+    public static String category(int length, int width, int height, int mass){
+        String ansBulky = "";
+        String ansHeavy = "";
+        String ans = "";
+        
+        if(length >= 10000 || width >= 10000 || height >= 10000 || (long)length*width*height >= 1000000000){
+            ansBulky = "" + "Bulky";
+            ans = "" + "Bulky";
+        }
+
+        if(mass >= 100){
+            ansHeavy= "" + "Heavy";
+            ans = "" + "heavy";
+        }
+
+        if(ansHeavy.equalsIgnoreCase("Heavy") && ansBulky.equalsIgnoreCase("Bulky")){
+            ans = "" + "Both";
+        }
+
+        if(ansHeavy.isEmpty() && ansBulky.isEmpty()){
+            ans = "" + "Neither";
+        }
+
+        if(ansHeavy.isEmpty() && ansBulky.equalsIgnoreCase("Bulky")){
+            ans = "" + "Bulky";
+        }
+
+        if(ansHeavy.equalsIgnoreCase("Heavy") && ansBulky.isEmpty()){
+            ans = "" + "Heavy";
+        }
+
+        return ans;
+    }
+}
