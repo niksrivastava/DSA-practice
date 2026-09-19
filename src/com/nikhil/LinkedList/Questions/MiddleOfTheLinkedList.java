f0314b55a85e67444b06866dbce761703eaf1f5e
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-19 06:50:33 +0530

    Solve: Middle of the Linked List

---
+++ src/com/nikhil/LinkedList/Questions/MiddleOfTheLinkedList.java
@@ -0,0 +1,28 @@
+package com.nikhil.LinkedList.Questions;
+import com.nikhil.LinkedList.*;
+public class MiddleOfTheLinkedList {
+    public static void main(String[] args) {
+        SLL list = new SLL();
+        list.insertLast(1);
+        list.insertLast(2);
+        list.insertLast(3);
+        list.insertLast(4);
+        list.insertLast(5);
+        list.insertLast(6);
+
+        SLL.Node ans = middle(list);
+        System.out.println(ans);
+    }
+
+    public static SLL.Node middle(SLL list){
+        SLL.Node slow = list.head;
+        SLL.Node fast = list.head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
