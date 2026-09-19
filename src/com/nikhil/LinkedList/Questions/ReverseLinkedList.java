6d296aad1425b4abe2698c4e9fae2fa17c7cc251
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-20 01:28:13 +0530

    Add: in-place reversal of the linked list

--- src/com/nikhil/LinkedList/Questions/ReverseLinkedList.java
+++ src/com/nikhil/LinkedList/Questions/ReverseLinkedList.java
@@ -11,6 +11,8 @@ public class ReverseLinkedList {
         list.insertLast(6);
         recursionReverse(list.head);
         list.display();
+        inPlaceReverse();
+        list.display();
     }
 
     static SLL list = new SLL();
@@ -26,5 +28,25 @@ public class ReverseLinkedList {
         list.tail.next = node;
         list.tail = node;
         list.tail.next = null;
+    }
+
+    public static void inPlaceReverse(){
+        if(list.size < 2){
+            return;
+        }
+
+        SLL.Node prev = null;
+        SLL.Node present = list.head;
+        SLL.Node next = present.next;
+
+        while(present != null){
+            present.next = prev;
+            prev = present;
+            present = next;
+            if(next != null){
+                next = next.next;
+            }
+        }
+        list.head = prev;
     }
 }
