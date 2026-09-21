d5ade6ee299efe288f0429248c3e584a88a59ec0
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-22 02:31:53 +0530

    Add: Largest Rectangle in Histogram

---
+++ src/com/nikhil/StacksAndQueues/Questions/LargestRectangleInHistogram.java
@@ -0,0 +1,52 @@
+package com.nikhil.StacksAndQueues.Questions;
+
+import java.util.Stack;
+
+public class LargestRectangleInHistogram {
+    public static void main(String[] args) {
+
+        int[] heights = {2,1,5,6,2,3};
+        int ans = largestArea(heights);
+        System.out.println(ans);
+        
+    }
+
+    public static int largestArea(int[] heights){
+
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+
+        stack.push(0);
+
+        for (int i = 1; i < heights.length; i++) {
+
+            while(!stack.isEmpty() && heights[i] < heights[stack.peek()]){
+
+                max = getMax(heights, stack, max, i);
+            }
+
+            stack.push(i);
+        }
+
+        int i = heights.length;
+
+            while(!stack.isEmpty()){
+                max = getMax(heights, stack, max, i);
+            }
+
+        return max;
+    }
+
+    private static int getMax(int[] arr, Stack<Integer> stack, int max, int i){
+
+        int area;
+        int popped = stack.pop();
+        if(stack.isEmpty()){
+            area = arr[popped] * i;
+
+        }else{
+            area = arr[popped] * (i - 1 - stack.peek());
+        }
+        return Math.max(max, area);
+    }
+}
