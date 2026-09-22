ee09b918a895c479557ecf77871ac3fa1c31c791
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-23 05:23:40 +0530

    Add: Delete the Middle Node of a Linked List

---
+++ src/com/nikhil/DsaPatterns/SlowAndFastPointer/DeleteMiddleNodeLinkedList.java
@@ -0,0 +1,56 @@
+package com.nikhil.DsaPatterns.SlowAndFastPointer;
+
+public class DeleteMiddleNodeLinkedList {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertLast(1);
+        list.insertLast(2);
+        list.insertLast(3);
+        list.insertLast(4);
+        list.insertLast(5);
+        list.insertLast(5);
+
+        LL.Node ans = deleteMiddle(list.head);
+        System.out.println(ans);
+    }
+
+    public static LL.Node deleteMiddle(LL.Node head){
+        LL.Node slow = head;
+        LL.Node fast = head;
+        int size = 0;  
+
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        while(fast != null){
+            size++;
+            fast = fast.next;
+        }
+
+        fast = head;
+
+        if(size % 2 == 0){
+            while(fast.next != null && fast.next.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            slow.next = slow.next.next;
+            return head;
+        }
+        else{
+            LL.Node dummy = new LL.Node(0);
+            dummy.next = head;
+            fast = dummy;
+            slow = dummy;
+            while(fast.next != null && fast.next.next != null){
+                    slow = slow.next;
+                    fast = fast.next.next;
+                }
+
+            slow.next = slow.next.next;
+            return head;
+        }
+    }   
+}
