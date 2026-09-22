910a31610769bbb07f4d3b68a3337142a7063a29
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-23 03:35:27 +0530

    Add: Remove Nth Node From End of List

---
+++ src/com/nikhil/DsaPatterns/SlowAndFastPointer/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,45 @@
+package com.nikhil.DsaPatterns.SlowAndFastPointer;
+
+public class RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertLast(1);
+        // list.insertLast(2);
+        // list.insertLast(3);
+        // list.insertLast(4);
+        // list.insertLast(5);
+
+        LL.Node ans = remove(list.head, 1);
+        System.out.println(ans);
+    }
+
+    public static LL.Node remove(LL.Node head, int n){
+
+        LL.Node temp = head;
+        int count = 0;
+
+        while (temp.next != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        if(n == count+1){
+            head = head.next;
+            return head;
+        }
+
+        n = count - n;
+        count = 1;
+        temp = head;
+
+        while (count <= n) {
+            count++;
+            temp = temp.next;
+        }
+
+        if(temp.next != null){
+            temp.next = temp.next.next;
+        }
+        return head;
+    }
+}
