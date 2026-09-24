b37a0961140cece7d31e5f3916be590cfdbe0af5
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-24 07:52:40 +0530

    Modified: Maximum Subarray Sum with One Deletion

--- src/com/nikhil/DsaPatterns/KadanesAlgorithm/MaximumSubarraySumWithOneDeletion.java
+++ src/com/nikhil/DsaPatterns/KadanesAlgorithm/MaximumSubarraySumWithOneDeletion.java
@@ -8,26 +8,42 @@ public class MaximumSubarraySumWithOneDeletion {
     }
 
     public static int oneDelete(int arr[]){
-        int noDelete = arr[0];
-        int oneDelete = Integer.MIN_VALUE;
+        // int noDelete = arr[0];
+        // int oneDelete = Integer.MIN_VALUE;
+        // int res = arr[0];
+
+        // for (int i = 1; i < arr.length; i++) {
+        //     int prevNoDelete = noDelete;
+        //     int prevOneDelete = oneDelete;
+
+        //     noDelete = Math.max(noDelete+arr[i], arr[i]);
+
+        //     int temp;
+        //     if(prevOneDelete == Integer.MIN_VALUE){
+        //         temp = arr[i];
+        //     }
+        //     else{
+        //         temp = prevOneDelete + arr[i];
+        //     }
+
+        //     oneDelete = Math.max(temp, prevNoDelete);
+        //     res = Math.max(res, Math.max(noDelete, oneDelete));
+        // }
+        // return res;
+
+        int power = 0;
+        int noPower = arr[0];
         int res = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
-            int prevNoDelete = noDelete;
-            int prevOneDelete = oneDelete;
+            int v1 = arr[i];
+            int v2 = noPower + arr[i];
+            int v3 = power + arr[i];
+            int v4 = noPower;
 
-            noDelete = Math.max(noDelete+arr[i], arr[i]);
-
-            int temp;
-            if(prevOneDelete == Integer.MIN_VALUE){
-                temp = arr[i];
-            }
-            else{
-                temp = prevOneDelete + arr[i];
-            }
-
-            oneDelete = Math.max(temp, prevNoDelete);
-            res = Math.max(res, Math.max(noDelete, oneDelete));
+            noPower = Math.max(v1, v2);
+            power = Math.max(v3, v4);
+            res = Math.max(res, Math.max(noPower, power));
         }
         return res;
     }
