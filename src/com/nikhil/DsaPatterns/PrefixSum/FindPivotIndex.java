61c9c5764312a818ef00e291d7136f8fbd92c6ba
niksrivastava <235146356+niksrivastava@users.noreply.github.com>
2026-09-25 18:00:08 +0530

    optimize: Find Pivot Index

--- src/com/nikhil/DsaPatterns/PrefixSum/FindPivotIndex.java
+++ src/com/nikhil/DsaPatterns/PrefixSum/FindPivotIndex.java
@@ -2,29 +2,31 @@ package com.nikhil.DsaPatterns.PrefixSum;
 
 public class FindPivotIndex {
     public static void main(String[] args) {
-        int[] nums= {1};
-        int ans = pivot(nums);
+        int[] arr= {1};
+        int ans = pivot(arr);
         System.out.println(ans);
-        int optimizedAns = optimized(nums);
+        int betterAns = better(arr);
+        System.out.println(betterAns);
+        int optimizedAns = optimized(arr);
         System.out.println(optimizedAns);
     }
-    public static int pivot(int[] nums){
+    public static int pivot(int[] arr){
         int i_sum = 0;
         int j_sum = 0;
 
-        if(nums.length == 1){
-            return nums.length-1;
+        if(arr.length == 1){
+            return arr.length-1;
         }
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
 
-            for (int j = i+1; j < nums.length; j++) {
-                if(j>nums.length-1){
+            for (int j = i+1; j < arr.length; j++) {
+                if(j>arr.length-1){
                     j_sum = 0;
                     break;
                 }else
                     {
-                    j_sum += nums[j];
+                    j_sum += arr[j];
                 }
             }
             if(i_sum == j_sum){
@@ -32,7 +34,7 @@ public class FindPivotIndex {
 
             }
             else{
-                i_sum += nums[i];
+                i_sum += arr[i];
             }
             j_sum = 0;
         }
@@ -40,24 +42,51 @@ public class FindPivotIndex {
         return -1;
     }
 
-    public static int optimized(int[] nums){
+    public static int better(int[] arr){
         int i = 1;
-        int j = nums.length-2;
-        int[] prefix = new int[nums.length];
-        int[] suffix = new int[nums.length];
+        int j = arr.length-2;
+        int[] prefix = new int[arr.length];
+        int[] suffix = new int[arr.length];
+
+        if(arr.length == 1){
+            return arr.length-1;
+        }
 
         while(i < prefix.length && j >= 0) {
-            prefix[i] = prefix[i-1] + nums[i-1];
+            prefix[i] = prefix[i-1] + arr[i-1];
             i++;
 
-            suffix[j] = suffix[j+1] + nums[j+1];
+            suffix[j] = suffix[j+1] + arr[j+1];
             j--;
         }
 
-        for(int k = 0; k < nums.length; k++){
+        for(int k = 0; k < arr.length; k++){
             if(prefix[k] == suffix[k]){
                 return k;
             }
+        }
+        return -1;
+    }
+
+    public static int optimized(int arr[]){
+        int prefix = 0;
+        int sum = 0;
+
+        if(arr.length == 1){
+            return arr.length-1;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int suffix = sum - prefix - arr[i];
+
+            if(prefix == suffix){
+                return i;
+            }
+            prefix += arr[i];
         }
         return -1;
     }
